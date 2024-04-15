69b039c632b72b92bdd37cce8be1c0152fe3c312
ThuNganlovePython <86398857+ThuNganPythonista@users.noreply.github.com>
2024-04-15 19:58:35 +0700

    Time: 55 ms (34.53%) | Memory: 48.3 MB (90.04%) - LeetSync

---
+++ 2747-apply-transform-over-each-element-in-array/apply-transform-over-each-element-in-array.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} arr
+ * @param {Function} fn
+ * @return {number[]}
+ */
+function map(arr, fn) {
+  const result = [];
+  for (let i = 0; i < arr.length; i++) {
+    result.push(fn(arr[i],i));
+  }
+  return result;
+}
+const arr = [1, 2, 3];
+const fn = function plusone(n) {
+  return n + 1;
+};
