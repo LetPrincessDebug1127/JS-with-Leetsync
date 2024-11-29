8065ea4e3da1428058bea2edb45ab69cb67cf168
NganSoSweet <86398857+ThuNganPythonista@users.noreply.github.com>
2024-11-29 23:36:51 +0700

    Time: 63 ms (8.32%) | Memory: 49.1 MB (32.88%) - LeetSync

--- 2746-filter-elements-from-array/filter-elements-from-array.js
+++ 2746-filter-elements-from-array/filter-elements-from-array.js
@@ -3,13 +3,14 @@
  * @param {Function} fn
  * @return {number[]}
  */
-function filter(arr, fn) {
-  const filteredArr = [];
-  for (let i = 0; i < arr.length; i++) {
-    if (fn(arr[i], i)) {
-      filteredArr.push(arr[i]);
+var filter = function(arr, fn) {
+    const filteredArr = [];
+    
+    for (let i = 0; i < arr.length; i++) {
+        if (fn(arr[i], i)) {
+            filteredArr.push(arr[i]);
+        }
     }
-  }
-  return filteredArr;
-}
-//
+    
+    return filteredArr;
+};
